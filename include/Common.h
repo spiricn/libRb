14b3f374137f52e09c93e0b51ddd6f4250f39d46
Nikola Spiric <nikola.spiric@rt-rk.com>
2016-06-18 14:47:44 +0200

    Add unix makefile

--- include/Common.h
+++ include/Common.h
@@ -5,6 +5,8 @@
 extern "C" {
 #endif
 
+#include <limits.h>
+
 #define RB_OK ( 0 )
 #define RB_TRUE ( 1 )
 #define RB_FALSE ( 0 )
@@ -16,7 +18,7 @@ extern "C" {
 #define RB_INVALID_ARG ( RB_ERROR_BASE - 3 )
 #define RB_TIMEOUT ( RB_ERROR_BASE - 4 )
 
-#define RB_WAIT_INFINITE ( LONG_MIN )
+#define RB_WAIT_INFINITE ( INT_MIN )
 
 #ifdef __cplusplus
 }
