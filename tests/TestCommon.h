a9f6da85f3ba1e2d1811c57f45d1c4776dcc3fd0
Nikola Spiric <nikola.spiric@rt-rk.com>
2017-07-11 22:58:18 +0200

    [test] Add assert equal int/float/str macros

--- tests/TestCommon.h
+++ tests/TestCommon.h
@@ -17,6 +17,31 @@
 
 #define ASSERT_EQUAL(expected, actual) ASSERT(expected == actual)
 
+// Integer
+#define ASSERT_EQUAL_INT32(expected, actual) \
+    do { \
+        if((expected) != (actual)){ \
+            RBLE("Assertion failure: %d != %d", (expected), (actual)); \
+            return -1; \
+        } \
+    } while(0)
+
+#define ASSERT_EQUAL_FLOAT(expected, actual) \
+    do { \
+        if((expected) != (actual)){ \
+            RBLE("Assertion failure: %f != %f", (expected), (actual)); \
+            return -1; \
+        } \
+    } while(0)
+
+#define ASSERT_EQUAL_STR(expected, actual) \
+    do { \
+    if(strcmp((expected), (actual)) != 0) { \
+        RBLE("Assertion failure: '%s' != '%s'", (expected), (actual)); \
+        return -1; \
+    } \
+    }while(0)
+
 #define ASSERT_NOT_EQUAL(a, b) ASSERT(a != b)
 
 #define ASSERT_NULL(val) val == NULL
